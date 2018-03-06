6e41491076e97b4b32e7c144a633b2012d106abc
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2018-03-06 10:54:33 -0500

    add json handler function

--- handler.go
+++ handler.go
@@ -1,6 +1,7 @@
 package urlshort
 
 import (
+	"encoding/json"
 	"net/http"
 
 	yaml "gopkg.in/yaml.v2"
@@ -26,7 +27,16 @@ func YAMLHandler(ymlBytes []byte, fallback http.Handler) (http.HandlerFunc, erro
 	return MapHandler(pathsToUrls, fallback), nil
 }
 
-func buildMap(pathsUrls []pathUrl) map[string]string {
+func JSONHandler(jsonBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathUrls, err := parseJson(jsonBytes)
+	if err != nil {
+		return nil, err
+	}
+	pathsToUrls := buildMap(pathUrls)
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func buildMap(pathsUrls []ymlPathUrl) map[string]string {
 	pathToUrls := make(map[string]string)
 	for _, pu := range pathUrls {
 		pathToUrls[pu.Path] = pu.URL
@@ -34,8 +44,8 @@ func buildMap(pathsUrls []pathUrl) map[string]string {
 	return pathsToUrls
 }
 
-func parseYAML(data []byte) ([]pathUrl, error) {
-	var pathUrls []pathUrl
+func parseYAML(data []byte) ([]ymlPathUrl, error) {
+	var pathUrls []ymlPathUrl
 	err := yaml.Unmarshal(data, &pathUrls)
 	if err != nil {
 		return nil, err
@@ -43,7 +53,21 @@ func parseYAML(data []byte) ([]pathUrl, error) {
 	return pathUrls, nil
 }
 
-type pathUrl struct {
+func parseJSON(data []byte) ([]jsonPathUrl, error) {
+	var pathUrls []jsonPathUrl
+	err := json.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
+}
+
+type ymlPathUrl struct {
 	Path string `yaml:"path"`
 	URL  string `yaml:"url"`
+}
+
+type jsonPathUrl struct {
+	Path string `json:"path"`
+	URL  string `json:"url"`
 }
